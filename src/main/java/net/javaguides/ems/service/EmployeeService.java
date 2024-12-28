8453f370765b8f07176ab34a9202e5eb737fcf4e
Noor <jawaidnoorafshan@gmail..com>
2024-12-28 20:57:10 +0530

    First Commit

---
+++ src/main/java/net/javaguides/ems/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package net.javaguides.ems.service;
+
+import java.util.List;
+
+import net.javaguides.ems.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+	EmployeeDto createEmployee(EmployeeDto employeeDto);
+	EmployeeDto getEmployeeById(Long employeeId);
+
+	List<EmployeeDto> getAllEmployees();
+	
+	EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmploye);
+	
+	void deleteEmployee(Long employeeId); 
+
+
+
+
+}
